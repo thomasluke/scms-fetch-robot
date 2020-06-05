f4cd4689fe2a601f955e73e02e109fd0ace9a389
thomasluke <thomasluke98@outlook.com>
2020-06-05 14:57:56 +1000

    Pick and place functionality

--- grasping/src/main.cpp
+++ grasping/src/main.cpp
@@ -52,6 +52,10 @@
 // TF2
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
+#include <geometry_msgs/Pose.h>
+
+#include <geometry_msgs/PoseArray.h>
+
 #include <iostream>
 
 #include <string>
@@ -92,7 +96,7 @@ void closedGripper(trajectory_msgs::JointTrajectory &posture)
     // END_SUB_TUTORIAL
 }
 
-void pick(moveit::planning_interface::MoveGroupInterface &move_group)
+void pick(moveit::planning_interface::MoveGroupInterface &move_group, std::string object, const geometry_msgs::Pose &bottle)
 {
     // BEGIN_SUB_TUTORIAL pick1
     // Create a vector of grasps to be attempted, currently only creating single grasp.
@@ -113,10 +117,9 @@ void pick(moveit::planning_interface::MoveGroupInterface &move_group)
     grasps[0].grasp_pose.header.frame_id = frame_id;
     tf2::Quaternion orientation;
     orientation.setRPY(0, 0, 0);
+    grasps[0].grasp_pose.pose = bottle;
     grasps[0].grasp_pose.pose.orientation = tf2::toMsg(orientation);
-    grasps[0].grasp_pose.pose.position.x = 0.45;
-    grasps[0].grasp_pose.pose.position.y = 0;
-    grasps[0].grasp_pose.pose.position.z = 1.2;
+    grasps[0].grasp_pose.pose.position.x += -0.2; // grasps[0].grasp_pose.pose.position.x = grasps[0].grasp_pose.pose.position.x -0.2;
 
     // Setting pre-grasp approach
     // ++++++++++++++++++++++++++
@@ -153,11 +156,59 @@ void pick(moveit::planning_interface::MoveGroupInterface &move_group)
     // Set support surface as table1.
     //move_group.setSupportSurfaceName("table1");
     // Call pick to pick up the object using the grasps given
-    move_group.pick("object", grasps);
+    move_group.pick(object, grasps);
     //END_SUB_TUTORIAL
 }
 
-void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface &planning_scene_interface)
+void place(moveit::planning_interface::MoveGroupInterface &group, std::string object, const geometry_msgs::Pose &bar)
+{
+    // BEGIN_SUB_TUTORIAL place
+    // TODO(@ridhwanluthra) - Calling place function may lead to "All supplied place locations failed. Retrying last
+    // location in
+    // verbose mode." This is a known issue and we are working on fixing it. |br|
+    // Create a vector of placings to be attempted, currently only creating single place location.
+    std::vector<moveit_msgs::PlaceLocation> place_location;
+    place_location.resize(1);
+
+    // Setting place location pose
+    // +++++++++++++++++++++++++++
+    place_location[0].place_pose.header.frame_id = frame_id;
+    tf2::Quaternion orientation;
+    orientation.setRPY(0, 0, 0);
+    place_location[0].place_pose.pose = bar;
+    place_location[0].place_pose.pose.orientation = tf2::toMsg(orientation);
+
+    // // Setting pre-place approach
+    // // ++++++++++++++++++++++++++
+    // /* Defined with respect to frame_id */
+    place_location[0].pre_place_approach.direction.header.frame_id = frame_id;
+    // /* Direction is set as negative z axis */
+    place_location[0].pre_place_approach.direction.vector.z = -1.0;
+    place_location[0].pre_place_approach.min_distance = 0.095;
+    place_location[0].pre_place_approach.desired_distance = 0.115;
+
+    // // Setting post-grasp retreat
+    // // ++++++++++++++++++++++++++
+    // /* Defined with respect to frame_id */
+    place_location[0].post_place_retreat.direction.header.frame_id = frame_id;
+    // /* Direction is set as negative y axis */
+    place_location[0].post_place_retreat.direction.vector.y = -1.0;
+    place_location[0].post_place_retreat.min_distance = 0.1;
+    place_location[0].post_place_retreat.desired_distance = 0.25;
+
+    // Setting posture of eef after placing object
+    // +++++++++++++++++++++++++++++++++++++++++++
+    /* Similar to the pick case */
+    openGripper(place_location[0].post_place_posture);
+
+    // Set support surface as table2.
+    //group.setSupportSurfaceName("table2");
+    // Call place to place the object using the place locations given.
+    group.place(object, place_location);
+    // END_SUB_TUTORIAL
+}
+
+void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface &planning_scene_interface, const geometry_msgs::Pose &bottle)
 {
     // BEGIN_SUB_TUTORIAL table1
     //
@@ -165,7 +216,7 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface &pla
     // ^^^^^^^^^^^^^^^^^^^^
     // Create vector to hold 3 collision objects.
     std::vector<moveit_msgs::CollisionObject> collision_objects;
-    collision_objects.resize(1);
+    collision_objects.resize(3);
 
     // BEGIN_SUB_TUTORIAL object
     // Define the object that we will be manipulating
@@ -182,17 +233,62 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface &pla
 
     /* Define the pose of the object. */
     collision_objects[0].primitive_poses.resize(1);
-    collision_objects[0].primitive_poses[0].position.x = 0.65;
-    collision_objects[0].primitive_poses[0].position.y = 0;
-    collision_objects[0].primitive_poses[0].position.z = 1.18;
+    collision_objects[0].primitive_poses[0] = bottle;
+
     // END_SUB_TUTORIAL
 
     // ADD ALL THE COLLISION OBJECTS/OBSTACLES HERE
 
     collision_objects[0].operation = collision_objects[0].ADD;
 
+    // Add the first table where the cube will originally be kept.
+    collision_objects[1].id = "table1";
+    collision_objects[1].header.frame_id = "base_link";
+
+    /* Define the primitive and its dimensions. */
+    collision_objects[1].primitives.resize(1);
+    collision_objects[1].primitives[0].type = collision_objects[0].primitives[0].BOX;
+    collision_objects[1].primitives[0].dimensions.resize(3);
+    collision_objects[1].primitives[0].dimensions[0] = 0.2;
+    collision_objects[1].primitives[0].dimensions[1] = 0.4;
+    collision_objects[1].primitives[0].dimensions[2] = 0.1;
+
+    /* Define the pose of the table. */
+    collision_objects[1].primitive_poses.resize(1);
+    collision_objects[1].primitive_poses[0].position.x = 0.8;
+    collision_objects[1].primitive_poses[0].position.y = 0;
+    collision_objects[1].primitive_poses[0].position.z = 0.95;
+    collision_objects[1].primitive_poses[0].orientation.w = 1.0;
+    // END_SUB_TUTORIAL
+
+    collision_objects[1].operation = collision_objects[1].ADD;
+
+    // BEGIN_SUB_TUTORIAL table2
+    // Add the second table where we will be placing the cube.
+    collision_objects[2].id = "table2";
+    collision_objects[2].header.frame_id = "base_link";
+
+    /* Define the primitive and its dimensions. */
+    collision_objects[2].primitives.resize(1);
+    collision_objects[2].primitives[0].type = collision_objects[1].primitives[0].BOX;
+    collision_objects[2].primitives[0].dimensions.resize(3);
+    collision_objects[2].primitives[0].dimensions[0] = 0.4;
+    collision_objects[2].primitives[0].dimensions[1] = 0.2;
+    collision_objects[2].primitives[0].dimensions[2] = 0.4;
+
+    /* Define the pose of the table. */
+    collision_objects[2].primitive_poses.resize(1);
+    collision_objects[2].primitive_poses[0].position.x = 0.3;
+    collision_objects[2].primitive_poses[0].position.y = 0.5;
+    collision_objects[2].primitive_poses[0].position.z = 0.4;
+    collision_objects[2].primitive_poses[0].orientation.w = 1.0;
+    // END_SUB_TUTORIAL
+
+    collision_objects[2].operation = collision_objects[2].ADD;
+
     planning_scene_interface.applyCollisionObjects(collision_objects);
-   // planning_scene_interface.removeCollisionObjects(collision_objects);
+
+    // planning_scene_interface.removeCollisionObjects(collision_objects);
 
     // moveit_msgs::AttachedCollisionObject attached_object;
 
@@ -205,12 +301,45 @@ void addCollisionObjects(moveit::planning_interface::PlanningSceneInterface &pla
     // attached_object.touch_links = std::vector<std::string>{"gripper", "r_gripper_finger_joint", "l_gripper_finger_joint"};
 }
 
+void addBottleObject(moveit::planning_interface::PlanningSceneInterface &planning_scene_interface, std::string name, geometry_msgs::Pose &bottle)
+{
+    moveit_msgs::CollisionObject collision_object;
+
+    collision_object.header.frame_id = "base_link";
+    collision_object.id = name;
+
+    /* Define the primitive and its dimensions. */
+    collision_object.primitives.resize(1);
+    collision_object.primitives[0].type = collision_object.primitives[0].BOX;
+    collision_object.primitives[0].dimensions.resize(3);
+    collision_object.primitives[0].dimensions[0] = 0.07;
+    collision_object.primitives[0].dimensions[1] = 0.07;
+    collision_object.primitives[0].dimensions[2] = 0.20;
+
+    /* Define the pose of the object. */
+    collision_object.primitive_poses.resize(1);
+    collision_object.primitive_poses[0] = bottle;
+    collision_object.operation = collision_object.ADD;
+
+    planning_scene_interface.applyCollisionObject(collision_object);
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "demo");
     ros::NodeHandle node_handle;
     ros::AsyncSpinner spinner(1);
     spinner.start();
+
+    geometry_msgs::Pose bottle;
+    bottle.position.x = 0.65;
+    bottle.position.y = 0;
+    bottle.position.z = 1.2;
+
+    geometry_msgs::Pose bar;
+    bar.position.x = 0.5;
+    bar.position.y = 0.3;
+    bar.position.z = 0.8;
 
     ros::WallDuration(1.0).sleep(); //< Uses system time to delay for 1.0 second.
 
@@ -220,13 +349,15 @@ int main(int argc, char **argv)
 
     gripper.setPlanningTime(45.0);
 
-    addCollisionObjects(planning_scene_interface);
+    addCollisionObjects(planning_scene_interface, bottle);
 
     ros::WallDuration(1.0).sleep(); //< Uses system time to delay for 1.0 second.
 
-    pick(arm);
+    pick(arm, "object", bottle);
 
     ros::WallDuration(1.0).sleep(); //< Uses system time to delay for 1.0 second.
+
+    place(arm, "object", bar);
 
     ros::shutdown();
 
